70ca4d566ffd9a7faf35dfbf75fb5e9fafd84df7
wabarr <wabarr@gmail.com>
2014-03-28 11:01:54 -0500

    added basic example of AJAX post

--- ajax_example/urls.py
+++ ajax_example/urls.py
@@ -7,6 +7,7 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'ajax_example.views.home'),
     # url(r'^blog/', include('blog.urls')),
+    url(r'^ajax_post_view/', 'ajax_example.views.ajax_post_view'),
     url(r'^ajax_results/(?P<type>.+)$','ajax_example.views.ajax_results'),
     url(r'^admin/', include(admin.site.urls)),
 )
--- ajax_example/views.py
+++ ajax_example/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.http import Http404, HttpResponse
+from django.forms.models import modelform_factory
 
 from ajax_example.models import item
 
 def home(request):
     dropdown_options = item.objects.values("type").distinct()
+    add_item_form = modelform_factory(item)
     return render_to_response("basic_template.html",
-                                {"dropdown_options": dropdown_options},
+                                {"dropdown_options": dropdown_options, "add_item_form":add_item_form},
                                 context_instance = RequestContext(request)
     )
 
@@ -21,5 +23,26 @@ def ajax_results(request, type=None):
                 results += "<tr><td>" + each.name + "</td></tr>"
             results += "</table>"
             return HttpResponse(results)
+    else:
+        return HttpResponse("you aint ajax")
+
+def ajax_post_view(request):
+    if request.is_ajax():
+
+        if request.method == "POST":
+            try:
+                itemInDB = item.objects.get(name__exact=request.POST["name"]) #will throw error if does not exist
+                return HttpResponse("Error:There is already an item with this name in the database")
+            except: #if it isn't there yet
+                try:
+                    newItem = item(name=request.POST["name"], type=request.POST["type"])
+                    newItem.save()
+                    return HttpResponse("Successfully added to the database")
+                except:
+                    return HttpResponse("Error:Item could not be saved.")
+
+        else: #if not POST
+            return HttpResponse("Error:Request method is not POST")
+
     else:
         return HttpResponse("you aint ajax")
